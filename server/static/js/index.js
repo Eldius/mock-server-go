222487c99882a415f1825d6b0cb5109b4cc04294
Eldius <eldiosantos@gmail.com>
2022-04-25 20:03:24 -0300

    Changing from XMLHttpRequest to fetch

--- server/static/js/index.js
+++ server/static/js/index.js
@@ -2,111 +2,114 @@
 (document.onload = function () {
 
     function fillRoutesFile() {
-        let xmlHttp = new XMLHttpRequest();
-        let respBody = "";
-        xmlHttp.open("GET", "/route", false);
-        xmlHttp.setRequestHeader("Accept", "application/yaml")
-        xmlHttp.send(null);
-        respBody = xmlHttp.responseText;
-        document.querySelector("#routes-config-file").innerHTML = respBody;
+        var headers = new Headers();
+        headers.set('Accept', 'application/yaml');
+        var cfg = {
+            method: 'GET',
+            headers: headers,
+        };
 
-        let link = document.createElement("a");
-        link.classList = ["btn btn-primary"]
-        link.href = "data:application/yaml;base64," + btoa(respBody);
-        link.download = "routes.yaml";
-        link.innerText = "Download config file here";
+        fetch('/route', cfg).then(response => response.text())
+            .then(response => {
+                document.querySelector("#routes-config-file").innerHTML = response;
 
-        document.querySelector("#config-file-card").appendChild(link);
+                let link = document.createElement("a");
+                link.classList = ["btn btn-primary"]
+                link.href = "data:application/yaml;base64," + btoa(response);
+                link.download = "routes.yaml";
+                link.innerText = "Download config file here";
+        
+                document.querySelector("#config-file-card").appendChild(link);
+            }).catch(function (err) {
+                // There was an error
+                console.warn('Failed to fetch routes def file.', err);
+            });
     }
 
     function fillRoutesList() {
-        let xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", "/route", false);
-        xmlHttp.send(null);
-        JSON.parse(xmlHttp.response).routes.forEach(element => {
-            var li = document.createElement("li");
-            li.classList = ["list-group-item"];
-            var text = document.createTextNode(`[${element.method}] ${element.path}`);
-            li.appendChild(text);
-
-            var element = document.querySelector("#my-routes-list");
-            element.appendChild(li);
-        });
+        fetch('/route').then(response => response.json())
+            .then(response => {
+                response.routes.forEach(element => {
+                    var li = document.createElement("li");
+                    li.classList = ["list-group-item"];
+                    var text = document.createTextNode(`[${element.method}] ${element.path}`);
+                    li.appendChild(text);
+        
+                    var element = document.querySelector("#my-routes-list");
+                    element.appendChild(li);
+                });
+            }).catch(function (err) {
+                // There was an error
+                console.warn('Failed to fetch routes def.', err);
+            });
     }
 
     function fillRequestList() {
-        let xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", "/request", false);
-        xmlHttp.send(null);
-        JSON.parse(xmlHttp.response).forEach(el => {
-            let tr = document.createElement("tr");
-            let colID = document.createElement("th"); // ID
-            colID.scope = ["row"];
-            colID.innerHTML = el.id;
-            tr.appendChild(colID);
+        fetch('/request').then(response => response.json())
+            .then(response => {
+                response.forEach(el => {
+                    let tr = document.createElement("tr");
+                    let colID = document.createElement("th"); // ID
+                    colID.scope = ["row"];
+                    colID.innerHTML = el.id;
+                    tr.appendChild(colID);
 
-            let colReqId = tr.appendChild(document.createElement("td")); // ReqID
-            colReqId.innerHTML = el.reqId;
-            tr.appendChild(colReqId);
+                    let colReqId = tr.appendChild(document.createElement("td")); // ReqID
+                    colReqId.innerHTML = el.reqId;
+                    tr.appendChild(colReqId);
 
-            let colReqDate = tr.appendChild(document.createElement("td")); // ReqDate
-            colReqDate.innerHTML = el.requestDate;
-            tr.appendChild(colReqDate);
+                    let colReqDate = tr.appendChild(document.createElement("td")); // ReqDate
+                    colReqDate.innerHTML = el.requestDate;
+                    tr.appendChild(colReqDate);
 
-            let colMothod = document.createElement("th"); // Method
-            colMothod.innerHTML = el.request.method;
-            tr.appendChild(colMothod);
+                    let colMothod = document.createElement("th"); // Method
+                    colMothod.innerHTML = el.request.method;
+                    tr.appendChild(colMothod);
 
-            let colPath = document.createElement("th"); // Path
-            colPath.innerHTML = el.request.path;
-            tr.appendChild(colPath);
+                    let colPath = document.createElement("th"); // Path
+                    colPath.innerHTML = el.request.path;
+                    tr.appendChild(colPath);
 
-            let colRequest = document.createElement("th"); // Request
-            colRequest.innerHTML = el.request.body;
-            tr.appendChild(colRequest);
+                    let colRequest = document.createElement("th"); // Request
+                    colRequest.innerHTML = el.request.body;
+                    tr.appendChild(colRequest);
 
-            let colRequestHeaders = document.createElement("th"); // Request Headers
-            let divReqHeaders = document.createElement("div");
-            colRequestHeaders.appendChild(divReqHeaders);
-            Object.entries(el.request.headers != null ? el.request.headers : []).forEach(entry => {
-                const [key, values] = entry;
-                console.log(key, values);
-                let p = document.createElement("p");
-                p.innerHTML = key + ": " + values;
-                divReqHeaders.appendChild(p);
-            });
-            tr.appendChild(colRequestHeaders);
+                    let colRequestHeaders = document.createElement("th"); // Request Headers
+                    let divReqHeaders = document.createElement("div");
+                    colRequestHeaders.appendChild(divReqHeaders);
+                    Object.entries(el.request.headers != null ? el.request.headers : []).forEach(entry => {
+                        const [key, values] = entry;
+                        let p = document.createElement("p");
+                        p.innerHTML = key + ": " + values;
+                        divReqHeaders.appendChild(p);
+                    });
+                    tr.appendChild(colRequestHeaders);
 
-            let colResponse = document.createElement("th"); // Response
-            colResponse.innerHTML = el.response.body;
-            tr.appendChild(colResponse);
+                    let colResponse = document.createElement("th"); // Response
+                    colResponse.innerHTML = el.response.body;
+                    tr.appendChild(colResponse);
 
-            let colResponseHeaders = document.createElement("th"); // Response Headers
-            let divResHeaders = document.createElement("div");
-            colResponseHeaders.appendChild(divResHeaders);
-            console.log(JSON.stringify(el.response.headers));
-            Object.entries(el.response.headers != null ? el.response.headers : []).forEach(entry => {
-                const [key, values] = entry;
-                console.log(key, values);
-                let p = document.createElement("p");
-                p.innerHTML = key + ": " + values;
-                divResHeaders.appendChild(p);
-            });
-            tr.appendChild(colResponseHeaders);
+                    let colResponseHeaders = document.createElement("th"); // Response Headers
+                    let divResHeaders = document.createElement("div");
+                    colResponseHeaders.appendChild(divResHeaders);
+                    Object.entries(el.response.headers != null ? el.response.headers : []).forEach(entry => {
+                        const [key, values] = entry;
+                        let p = document.createElement("p");
+                        p.innerHTML = key + ": " + values;
+                        divResHeaders.appendChild(p);
+                    });
+                    tr.appendChild(colResponseHeaders);
 
-            let colStatusCode = document.createElement("th"); // Code
-            colStatusCode.innerHTML = el.response.code;
-            tr.appendChild(colStatusCode);
+                    let colStatusCode = document.createElement("th"); // Code
+                    colStatusCode.innerHTML = el.response.code;
+                    tr.appendChild(colStatusCode);
 
-            document.querySelector("#requests-tbody").appendChild(tr);
+                    document.querySelector("#requests-tbody").appendChild(tr);
+                });
+        }).catch(function (err) {
+            // There was an error
+            console.warn('Failed to fetch requests.', err);
         });
-    }
-
-    function ajaxRequest() {
-        var url = '/request'
-        $.get(url).then(function (res) {
-            params.success(res);
-        })
     }
 
     fillRoutesFile();
