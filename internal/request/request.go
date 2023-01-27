e473492b2cf840b92302095c3cf5d8b6fed51aaa
eldius <eldiosantos@gmail.com>
2023-01-26 21:17:36 -0300

    Removed deprecated function usage

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,70 +1,73 @@
 package request
 
 import (
-    "bytes"
-    "io/ioutil"
-    "net/http"
-    "strings"
-    "time"
+	"bytes"
+	"io"
+	"net/http"
+	"strings"
+	"time"
 
-    "github.com/google/uuid"
-    "github.com/sirupsen/logrus"
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
 )
 
 type Headers map[string]string
 
 type RequestRecord struct {
-    Path    string  `json:"path"`
-    Method  string  `json:"method"`
-    Headers Headers `json:"headers"`
-    Body    string  `json:"body"`
+	Path    string  `json:"path"`
+	Method  string  `json:"method"`
+	Headers Headers `json:"headers"`
+	Body    string  `json:"body"`
 }
 
 type ResponseRecord struct {
-    Headers Headers `json:"headers"`
-    Code    int     `json:"code"`
-    Body    string  `json:"body"`
+	Headers Headers `json:"headers"`
+	Code    int     `json:"code"`
+	Body    string  `json:"body"`
 }
 
 type Record struct {
-    ID          int            `json:"id"`
-    RequestDate time.Time      `json:"requestDate"`
-    ReqID       uuid.UUID      `json:"reqId"`
-    Request     RequestRecord  `json:"request"`
-    Response    ResponseRecord `json:"response"`
+	ID          int            `json:"id"`
+	RequestDate time.Time      `json:"requestDate"`
+	ReqID       uuid.UUID      `json:"reqId"`
+	Request     RequestRecord  `json:"request"`
+	Response    ResponseRecord `json:"response"`
 }
 
 func NewRecord(r *http.Request) *Record {
-    bodyReader := r.Body
-    defer bodyReader.Close()
-    body, err := ioutil.ReadAll(bodyReader)
-    if err != nil {
-        log.WithError(err).Printf("Failed to read request body")
-        return nil
-    }
-    r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+	bodyReader := r.Body
+	defer func() {
+		_ = bodyReader.Close()
+	}()
 
-    log.WithFields(logrus.Fields{
-        "body": string(body),
-    }).Debug("request body")
+	body, err := io.ReadAll(bodyReader)
+	if err != nil {
+		log.WithError(err).Printf("Failed to read request body")
+		return nil
+	}
+	r.Body = io.NopCloser(bytes.NewBuffer(body))
 
-    h := make(map[string]string)
-    for k, v := range r.Header {
-        h[k] = strings.Join(v, ",")
-    }
-    return &Record{
-        ReqID:       uuid.New(),
-        RequestDate: time.Now(),
-        Request: RequestRecord{
-            Path:    r.URL.Path,
-            Method:  r.Method,
-            Body:    string(body),
-            Headers: Headers(h),
-        },
-    }
+	log.WithFields(logrus.Fields{
+		"body": string(body),
+	}).Debug("request body")
+
+	h := make(map[string]string)
+	for k, v := range r.Header {
+		h[k] = strings.Join(v, ",")
+	}
+	return &Record{
+		ReqID:       uuid.New(),
+		RequestDate: time.Now(),
+		Request: RequestRecord{
+			Path:    r.URL.Path,
+			Method:  r.Method,
+			Body:    string(body),
+			Headers: Headers(h),
+		},
+	}
 }
 
 func (r *Record) AddResponse(response ResponseRecord) *Record {
-    r.Response = response
-    return r
+	r.Response = response
+	return r
 }
